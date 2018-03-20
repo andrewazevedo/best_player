c0052572c385af3747fb6187f97fad4c46e96dc3
Andrew Azevedo <drew.hack42@gmail.com>
2018-03-20 10:21:46 -0300

    creates best_player project; configs Airbnb ESLint

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>The best React Native Player you've ever seen</Text>
+      </View>
+    );
+  }
+}
+
+export default App
